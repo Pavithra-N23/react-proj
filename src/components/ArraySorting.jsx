10b1b02692748dc56ae169cf4e2ab903fbde61e4
pavihra-N <pavithran7022@gmail.com>
2025-09-23 09:37:08 +0200

    Basic sorting techniques

--- src/components/ArraySorting.jsx
+++ src/components/ArraySorting.jsx
@@ -1,12 +1,32 @@
-import { builInSorting } from "../js/Practise.js";
+import { ASC, DESC } from "../js/Constants.js";
+import { bubbleSorting, builInSorting, insertionSorting, selectionSorting } from "../js/Practise.js";
 
 const ArraySorting = () => {
   const a = [1, 2, 6, 3, 5];
   const sortedArray = builInSorting([...a]); // spread so original not mutated
+  const bubbleSortAsc = bubbleSorting([2,1,5,4], ASC);
+    const bubbleSortDesc = bubbleSorting([2,1,5,4], DESC);
+    const selectionSort = selectionSorting([2,1,5,3], ASC);
+    const insertionSort = insertionSorting([5,1,2,9], ASC);
+
 
   return (
     <div>
-      Sorted Array: {sortedArray.join(", ")}
+      <div>
+        Sorted Array: {sortedArray.join(", ")}
+      </div>
+      <div>
+        BubbleSort ascending: {bubbleSortAsc.join(", ")};
+      </div>
+      <div>
+        BubbleSort descending: {bubbleSortDesc.join(", ")};
+      </div>
+        <div>
+        Selection sort descending: {selectionSort.join(", ")};
+      </div>
+        <div>
+        Insertion sort descending: {insertionSort.join(", ")};
+      </div>
     </div>
   );
 }
--- src/js/Practise.js
+++ src/js/Practise.js
@@ -1,3 +1,6 @@
+import { ASC } from "./Constants";
+
+// import { startTransition } from "react";
 export const reverseArray = (arr) => {
   let left = 0;
   let right = arr.length - 1;
@@ -17,6 +20,67 @@ export const reverseArray = (arr) => {
 
 
 export const builInSorting = (arr) => {
-  arr.sort((a, b) => a-b);
+  arr.sort((a, b) => a - b);
   return arr;
 }
+
+
+// Sorting
+
+// BubbleSort
+export const bubbleSorting = (arr, type) => {
+  let n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (type === ASC) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      } else {
+        if (arr[j] < arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+
+    }
+  }
+  console.log(arr);
+  return arr;
+
+}
+
+export const selectionSorting = (arr, type) => {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min])
+        min = j;
+    }
+    [arr[min], arr[i]] = [arr[i], arr[min]];
+  }
+  return arr;
+}
+
+export const insertionSorting = (arr, type) => {
+  let n = arr.length;
+  for (let i = 1; i < n; i++) {
+    let j = i - 1;
+    let key = arr[i];
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+
+
+
+// docker start
+// docker exec -it image_id bash
+// npm run dev -- --port=5174  ///For a specific port
+
